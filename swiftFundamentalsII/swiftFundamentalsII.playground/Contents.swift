30d28f1181663d0808224baf4b791e0e8f118079
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-02-08 22:26:46 -0800

    final

---
+++ swiftFundamentalsII/swiftFundamentalsII.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//for i in 1...255 {
+//    print(i)
+//}
+
+//
+for i in 1...100
+{
+    if i % 3 == 0 || i % 5 == 0 {
+         print(i)
+     }
+ 
+ }
+    
+//for i in 1...100
+//{
+//    if i % 3 == 0{
+//         print("Fizz")
+//     }
+//    else if i % 5 == 0{
+//         print("Buzz")
+//     }
+//    else if i % 15 == 0 {
+//        print("FizzBuzz")
+//
+//    }
+// }
+
+
+
